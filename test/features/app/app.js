99c2573dbd545d71610b90f8a42a4234f61f6205
STran <stran@wikimedia.org>
2021-11-23 02:29:54 -0800

    Clean up boilerplate
    Change-Id: I32ba998b56462733ff0e2ee148465a2428ac0cad

--- test/features/app/app.js
+++ test/features/app/app.js
@@ -51,31 +51,4 @@ describe('express app', function () {
             assert.deepEqual(res.headers['content-security-policy'], 'default-src \'self\'; object-src \'none\'; media-src \'none\'; img-src \'none\'; style-src \'none\'; base-uri \'self\'; frame-ancestors \'self\'');
         });
     });
-
-    it('should get static content gzipped', () => {
-        return preq.get({
-            uri: `${server.config.uri}static/index.html`,
-            headers: {
-                'accept-encoding': 'gzip, deflate'
-            }
-        }).then((res) => {
-            assert.deepEqual(res.status, 200);
-            // if there is no content-length, the reponse was gzipped
-            assert.deepEqual(res.headers['content-length'], undefined,
-                'Did not expect the content-length header!');
-        });
-    });
-
-    it('should get static content uncompressed', () => {
-        return preq.get({
-            uri: `${server.config.uri}static/index.html`,
-            headers: {
-                'accept-encoding': ''
-            }
-        }).then((res) => {
-            const contentEncoding = res.headers['content-encoding'];
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(contentEncoding, undefined, 'Did not expect gzipped contents!');
-        });
-    });
 });
