0d49a5c55ab99da692d020e66af6cdf90ccf0d7f
STran <stran@wikimedia.org>
2022-05-12 16:16:06 -0700

    Add data import to init-db
    - import data from gzip located at FEED_PATH on init
    
    Change-Id: Ibb35011d7dd69bc438b94f59697616871bd75c25

--- init-db.js
+++ init-db.js
@@ -2,21 +2,179 @@
 
 const fs = require('fs');
 const mariadb = require('mariadb');
+const zlib = require('zlib');
+const byline = require('byline');
 
-async function addTables() {
-    const connection = await mariadb.createConnection({
+async function getConnection() {
+    return await mariadb.createConnection({
         host: process.env.HOST,
         user: process.env.MYSQL_ROOT_USER,
         password: process.env.MYSQL_ROOT_PASSWORD,
         database: process.env.MYSQL_DATABASE,
         multipleStatements: true
        });
+}
 
+async function addTables() {
+    const connection = await getConnection();
     const schema = fs.readFileSync('./schema.sql', 'utf8');
-    const test = await connection.query(schema);
-    console.log(test); // [{ "1": 1 }]
-
+    await connection.query(schema);
     connection.end();
 }
 
-addTables();
+const entityTypes = {
+    UNKNOWN: 0,
+    DESKTOP: 1,
+    HEADLESS: 2,
+    IOT: 3,
+    MOBILE: 4
+};
+
+function getEntityTypes(types) {
+    // Assume only one in array
+    return entityTypes[types[0]];
+}
+
+const riskTypes = {
+    UNKNOWN: 0,
+    CALLBACK_PROXY: 1,
+    GEO_MISMATCH: 2,
+    LOGIN_BRUTEFORCE: 3,
+    TUNNEL: 4,
+    WEB_SCRAPING: 5
+};
+
+function getEntityRisks(risks) {
+    // Assume only one in array
+    return riskTypes[risks[0]];
+}
+
+const behaviorPkids = {};
+const proxyPkids = {};
+const tunnelPkids = {};
+
+let entryCount = 0;
+let processedEntries = 0;
+
+async function closeConnectionWhenDone(connection) {
+    if (processedEntries === entryCount) {
+        await connection.end();
+    } else {
+        setTimeout(function () {
+            closeConnectionWhenDone(connection);
+        }, 1000);
+    }
+}
+
+async function importData() {
+    const connection = await getConnection();
+    const lineReader = byline(fs.createReadStream(process.env.FEED_PATH).pipe(zlib.createGunzip()));
+
+    // Clear tables before processing
+    await connection.query(`DELETE FROM actor_data; 
+        DELETE FROM behaviors; DELETE FROM actor_data_behaviors;
+        DELETE FROM proxies; DELETE FROM actor_data_proxies;
+        DELETE FROM tunnels; DELETE FROM actor_data_tunnels;`);
+    lineReader.on('data', async (line) => {
+        entryCount += 1;
+        const entity = JSON.parse(line);
+        const queryObj = {
+            actor_data: {
+                ip: entity.ip,
+                org: entity.organization,
+                client_count: entity.client.count || 0,
+                types: entity.client.types ?
+                    getEntityTypes(entity.client.types) : entityTypes.UNKNOWN,
+                conc_geohash:
+                    entity.client.concentration && entity.client.concentration.geohash ? entity.client.concentration.geohash : '',
+                conc_city:
+                    entity.client.concentration && entity.client.concentration.city ? entity.client.concentration.city : '',
+                conc_state:
+                    entity.client.concentration && entity.client.concentration.state ? entity.client.concentration.state : '',
+                conc_country:
+                    entity.client.concentration && entity.client.concentration.country ? entity.client.concentration.country : '',
+                conc_skew:
+                    entity.client.concentration && entity.client.concentration.skew ?
+                    entity.client.concentration.skew : 0,
+                conc_density:
+                    entity.client.concentration && entity.client.concentration.density ?
+                    entity.client.concentration.density : 0,
+                countries: entity.client.countries || 0,
+                location_country: entity.location.country || '',
+                risks: getEntityRisks(entity.risks) || riskTypes.UNKNOWN
+            },
+            behaviors: entity.client.behaviors || [],
+            proxies: entity.client.proxies || [],
+            tunnels: entity.tunnels || []
+        };
+
+        // Add to actor_data
+        const pkid = await connection.query(
+            `INSERT INTO
+                actor_data (${Object.keys(queryObj.actor_data).toString()})
+            VALUES
+                ( ${JSON.stringify(Object.values(queryObj.actor_data)).slice(1, -1)} )`
+        );
+
+        // Add to behaviors
+        queryObj.behaviors.forEach(async (behavior) => {
+            const behaviorPkid = await connection.query(
+                'INSERT IGNORE INTO behaviors (behavior) VALUES (?);', [behavior]
+            );
+            if (behaviorPkid.insertId) {
+                behaviorPkids[behavior] = behaviorPkid.insertId;
+            }
+            await connection.query(
+                `INSERT INTO
+                    actor_data_behaviors (actor_data_id,behavior_id)
+                VALUES
+                    (${pkid.insertId}, ${behaviorPkids[behavior]})`
+            );
+        });
+
+        // Add to proxies
+        queryObj.proxies.forEach(async (proxy) => {
+            const proxyPkid = await connection.query(
+                'INSERT IGNORE INTO proxies (proxy) VALUES (?);', [proxy]
+            );
+            if (proxyPkid.insertId) {
+                proxyPkids[proxy] = proxyPkid.insertId;
+            }
+            await connection.query(
+                `INSERT INTO
+                    actor_data_proxies (actor_data_id,proxy_id)
+                VALUES
+                    (${pkid.insertId}, ${proxyPkids[proxy]})`
+            );
+        });
+
+        // Add to tunnels
+        queryObj.tunnels.forEach(async (tunnel) => {
+            const tunnelPkid = await connection.query(
+                'INSERT IGNORE INTO tunnels (tunnel) VALUES (?);', [tunnel]
+            );
+            if (tunnelPkid.insertId) {
+                tunnelPkids[tunnel] = tunnelPkid.insertId;
+            }
+            await connection.query(
+                `INSERT INTO
+                    actor_data_tunnels (actor_data_id,tunnel_id)
+                VALUES
+                    (${pkid.insertId}, ${tunnelPkids[tunnel]})`
+            );
+        });
+
+        processedEntries += 1;
+    });
+
+    lineReader.on('end', () => {
+        closeConnectionWhenDone(connection);
+      });
+}
+
+async function init() {
+    await addTables();
+    await importData();
+}
+
+init();
--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS actor_data (
    ip VARBINARY(128) NOT NULL,
    org VARBINARY(128),
    client_count INT UNSIGNED,
-   -- 1 = 'DESKTOP', 2 = 'HEADLESS', 3 = 'IOT', 4 = 'MOBILE'
-   types SET('1','2','3','4'),
+   -- 0 = 'UNKNOWN', 1 = 'DESKTOP', 2 = 'HEADLESS', 3 = 'IOT', 4 = 'MOBILE'
+   types SET('0','1','2','3','4'),
    conc_geohash VARBINARY(16),
    conc_city VARBINARY(32),
    conc_state VARBINARY(32),
@@ -13,40 +13,40 @@ CREATE TABLE IF NOT EXISTS actor_data (
    conc_density INT,
    countries INT,
    location_country VARBINARY(32),
-   -- 1 = 'CALLBACK_PROXY', 2 = 'GEO_MISMATCH', 3 = 'LOGIN_BRUTEFORCE', 4 = 'TUNNEL', 5 = 'WEB_SCRAPING'
-   risks SET('1','2','3','4','5'),
+   -- 0 = 'UNKNOWN' ,1 = 'CALLBACK_PROXY', 2 = 'GEO_MISMATCH', 3 = 'LOGIN_BRUTEFORCE', 4 = 'TUNNEL', 5 = 'WEB_SCRAPING'
+   risks SET('0','1','2','3','4','5'),
    PRIMARY KEY (pkid)
 );
 
 CREATE TABLE IF NOT EXISTS behaviors (
    pkid BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-   behaviors VARBINARY(64),
+   behavior VARBINARY(64) UNIQUE,
    PRIMARY KEY (pkid)
 );
 
 CREATE TABLE IF NOT EXISTS actor_data_behaviors (
    actor_data_id BIGINT UNSIGNED,
-   behaviors_id BIGINT UNSIGNED
+   behavior_id BIGINT UNSIGNED
 );
 
 CREATE TABLE IF NOT EXISTS proxies (
    pkid BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-   proxy VARBINARY(32),
+   proxy VARBINARY(128) UNIQUE,
    PRIMARY KEY (pkid)
 );
 
 CREATE TABLE IF NOT EXISTS actor_data_proxies (
    actor_data_id BIGINT UNSIGNED,
-   proxies_id BIGINT UNSIGNED
+   proxy_id BIGINT UNSIGNED
 );
 
 CREATE TABLE IF NOT EXISTS tunnels (
    pkid BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-   tunnels VARBINARY(32),
+   tunnel VARBINARY(256) UNIQUE,
    PRIMARY KEY (pkid)
 );
 
 CREATE TABLE IF NOT EXISTS actor_data_tunnels (
    actor_data_id BIGINT UNSIGNED,
-   tunnels_id BIGINT UNSIGNED
+   tunnel_id BIGINT UNSIGNED
 );
